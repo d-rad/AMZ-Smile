ee3c0a8f9724dd176f1291b860d27384dc389413
d-rad <50156535+d-rad@users.noreply.github.com>
2019-05-01 12:51:25 -0500

    Saving and reading button status from chrome storage now works

--- popup.js
+++ popup.js
@@ -1,19 +1,15 @@
-window.onload = function loadSwitch() {
-    var defaultValue = "true";
-    var updateState = chrome.storage.sync.get({ activeSwitch: defaultValue }, function (data) {
-        chrome.storage.sync.set({ activeSwitch: data.activeSwitch }, function () {
-
-        });
-    });
-    document.getElementById("activeSwitch").checked = updateState;
-}
+var defaultValue = true;
+chrome.storage.sync.get('activeSwitch', function (data) {
+  if (!data.hasOwnProperty('activeSwitch') || typeof data.activeSwitch === 'undefined')
+    data.activeSwitch = defaultValue;
+  document.getElementById("activeSwitch").checked = data.activeSwitch;
+});
 
 function storeSwitch() {
-    var switchState = document.getElementById("activeSwitch").checked;
-    chrome.storage.sync.set({ activeSwitch: switchState }, function () {
-    });
+  var switchState = document.getElementById('activeSwitch').checked;
+  chrome.storage.sync.set({activeSwitch: switchState}, function () {
+    console.log('activeSwitch saved');
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("activeSwitch").addEventListener("click", storeSwitch);
-});
+document.getElementById('activeSwitch').addEventListener('change', storeSwitch);
